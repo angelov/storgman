160298ec4da594f31ee42c629dca7e2e5970754e
Dejan Angelov <angelovdejan92@gmail.com>
2014-08-16 22:16:53 +0200

    formatting and making some dates human readable

--- public/static/assets/app.js
+++ public/static/assets/app.js
@@ -22,6 +22,40 @@ $(function(){
     $('.has-tooltip').tooltip();
 
     /**
+     * Formatting the dates and making them human-readable
+     */
+
+    $(".date-to-humanize").each(function() {
+        //console.log($(this).html());
+        var original = $(this).html();
+        var date = moment(original);
+        var now = moment();
+        var duration = moment.duration(now - date);
+
+        var prefix = '';
+        var suffix = '';
+        if (now.diff(date) < 0) {
+            prefix = 'in ';
+        } else {
+            suffix = ' ago';
+        }
+
+        $(this).html(prefix + duration.humanize() + suffix);
+        $(this).attr('title', original);
+    });
+
+    // this should be set before
+    var dateFormat = "DD MMM YYYY";
+
+    $(".date-to-format").each(function() {
+        var original = $(this).html();
+        var date = moment(original);
+
+        $(this).html(date.format(dateFormat));
+        $(this).attr('title', original);
+    });
+
+    /**
      * Deleting a member
      */
 
