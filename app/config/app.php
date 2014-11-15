894bd5ad0824e91badf947332a2d02cf2f1fb93d
Dejan Angelov <angelovdejan92@gmail.com>
2014-11-15 10:37:02 +0100

    update dependencies for Laravel 5

--- app/config/app.php
+++ app/config/app.php
@@ -113,7 +113,7 @@ return array(
         'Illuminate\Validation\ValidationServiceProvider',
         'Illuminate\View\ViewServiceProvider',
         'Illuminate\Workbench\WorkbenchServiceProvider',
-        'Barryvdh\TwigBridge\ServiceProvider',
+        'TwigBridge\ServiceProvider',
         'Way\Generators\GeneratorsServiceProvider',
         'Intervention\Image\ImageServiceProvider',
         'Angelov\Eestec\Platform\Eventing\EventingServiceProvider'
