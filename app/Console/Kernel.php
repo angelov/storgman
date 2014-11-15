11bc47a0edee3dce162d621a393704fb1a70ff04
Dejan Angelov <angelovdejan92@gmail.com>
2014-11-15 23:07:58 +0100

    added the missing console/kernel file

---
+++ app/Console/Kernel.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * EESTEC Platform for Local Committees
+ * Copyright (C) 2014, Dejan Angelov <angelovdejan92@gmail.com>
+ *
+ * This file is part of EESTEC Platform.
+ *
+ * EESTEC Platform is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EESTEC Platform is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EESTEC Platform.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package EESTEC Platform
+ * @copyright Copyright (C) 2014, Dejan Angelov <angelovdejan92@gmail.com>
+ * @license https://github.com/angelov/eestec-platform/blob/master/LICENSE
+ * @author Dejan Angelov <angelovdejan92@gmail.com>
+ */
+
+namespace App\Console;
+
+use Exception;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel {
+
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        
+    ];
+
+    /**
+     * Run the console application.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return int
+     */
+    public function handle($input, $output = null)
+    {
+        try
+        {
+            return parent::handle($input, $output);
+        }
+        catch (Exception $e)
+        {
+            $this->reportException($e);
+
+            $this->renderException($output, $e);
+
+            return 1;
+        }
+    }
+
+}
