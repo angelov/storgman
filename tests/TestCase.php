ec3be30a1fbe9408eee1a14845693f8222d7f763
Dejan Angelov <angelovdejan92@gmail.com>
2014-11-21 20:49:39 +0100

    fixed the app creating in the tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,17 +7,17 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
     use ModelHelpers;
 
     /**
-	 * Creates the application.
-	 *
-	 * @return \Symfony\Component\HttpKernel\HttpKernelInterface
-	 */
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
     public function createApplication()
     {
-        $unitTesting = true;
+        $app = require __DIR__.'/../bootstrap/app.php';
 
-        $testEnvironment = 'testing';
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
-        return require __DIR__.'/../../bootstrap/start.php';
+        return $app;
     }
 
     public function tearDown()
