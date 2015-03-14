66d780b0e0bb862ae26379511a360853e89428b6
Dejan Angelov <angelovdejan92@gmail.com>
2015-03-14 17:09:08 +0100

    buttons to share documents on fb/twitter

--- public/static/assets/app.js
+++ public/static/assets/app.js
@@ -609,4 +609,20 @@ $(function(){
         return false;
     });
 
+    /**
+     * Share document on Facebook or Twitter
+     */
+
+    $('.btn-fb-share').click(function(e) {
+        e.preventDefault();
+        window.open($(this).attr('href'), 'fbShareWindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 275) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+        return false;
+    });
+
+    $('.btn-twitter-share').click(function(e) {
+        e.preventDefault();
+        window.open($(this).attr('href'), 'twitterShareWindow', 'height=300, width=550, top=' + ($(window).height() / 2 - 275) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+        return false;
+    });
+
 });
