9207050be076576af1a0b585904da70cdb33112e
Angelov Dejan <angelovdejan92@gmail.com>
2015-10-25 02:06:07 +0100

    tests for the abstract monthly report class

---
+++ tests/Core/Reports/AbstractMonthlyReportTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Angelov\Eestec\Platform\Tests\Core\Reports;
+
+use Angelov\Eestec\Platform\Core\DateTime;
+use Angelov\Eestec\Platform\Core\Reports\AbstractMonthlyReport;
+use Angelov\Eestec\Platform\Tests\TestCase;
+use Mockery;
+
+class AbstractMonthlyReportTest extends TestCase
+{
+    /** @var AbstractMonthlyReport $report */
+    protected $report;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $from = DateTime::createFromFormat('Y-m-d', '2015-01-01');
+        $to = DateTime::createFromFormat('Y-m-d', '2015-03-01');
+
+        $this->report = $this->getMockForAbstractClass(AbstractMonthlyReport::class, [$from, $to]);
+    }
+
+    public function testHasMonthsArrayInitializedByDefault()
+    {
+        $combined = [
+            '2015-01' => 0,
+            '2015-02' => 0,
+            '2015-03' => 0
+        ];
+
+        $this->assertEquals($combined, $this->report->getMonths());
+    }
+
+    public function testCanGenerateMonthTitles()
+    {
+        $months = ['Jan 2015', 'Feb 2015', 'Mar 2015'];
+
+        $this->assertEquals($months, $this->report->getMonthsTitles());
+    }
+
+    public function testCanReturnTheValues()
+    {
+        $values = [0, 0 , 0];
+
+        $this->assertEquals($values, $this->report->getMonthsValues());
+    }
+}
