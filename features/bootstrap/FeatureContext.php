211c796f86328e8f342b765dced8ec6cd4f4937d
Dejan Angelov <angelovdejan92@gmail.com>
2016-02-05 02:26:07 +0100

    basic member authentication scenario

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,10 +1,14 @@
 <?php
 
+// Everything in this file is just for experimental purposes!
+
+use Angelov\Eestec\Platform\Members\Member;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
+use Illuminate\Contracts\Hashing\Hasher;
 
 /**
  * Defines application features from the specific context.
@@ -61,5 +65,61 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
         if ($this->getSession()->evaluateScript("return $('.modal').is(':visible')")) {
             throw new \Exception('The modal window did not disappear.');
         }
+    }
+
+    /**
+     * @Given /^there are the following users:$/
+     */
+    public function thereAreTheFollowingUsers(TableNode $table)
+    {
+        $faker = Faker\Factory::create();
+        $hasher = app()->make(Hasher::class);
+
+        foreach ($table as $row) {
+            $member = new Member();
+
+            $member->setEmail($row['email']);
+            $member->setPassword($hasher->make($row['password']));
+            $member->setFirstName($faker->firstName);
+            $member->setLastName($faker->lastName);
+            $member->setFaculty("Faculty");
+            $member->setFieldOfStudy("Field of study");
+            $member->setYearOfGraduation($faker->numberBetween(2015, 2018));
+
+            $member->setApproved(true);
+
+            $social = strtolower($member->getFirstName() . $member->getLastName());
+
+            if ($faker->boolean(60)) {
+                $member->setFacebook($social);
+            }
+
+            if ($faker->boolean(60)) {
+                $member->setTwitter($social);
+            }
+
+            if ($faker->boolean(60)) {
+                $member->setGooglePlus($social);
+            }
+
+            $member->setPhoneNumber($faker->phoneNumber);
+            $member->setWebsite("http://". $social .".com");
+
+            $birthday = $faker->dateTimeBetween("-20 years", "now");
+
+            $member->setBirthday($birthday);
+            $member->setBoardMember($row['board'] == "true");
+
+            $member->save();
+        }
+    }
+
+    /**
+     * @Given /^I am on the login page$/
+     */
+    public function iAmOnTheLoginPage()
+    {
+        $loginPath = route('auth');
+        $this->visit($loginPath);
     }
 }
---
+++ features/members/account/sign_in.feature
@@ -0,0 +1,17 @@
+Feature: Authentication
+  In order to use the application
+  As an organization member
+  I need to sign in
+
+  Background:
+    Given there are the following users:
+      | email               | password | board |
+      | angelov@example.org | 123456   | false |
+
+  Scenario: Login with valid credentials
+    Given I am on the login page
+    When I fill in the following:
+      | Email address | angelov@example.org |
+      | Password      | 123456              |
+    And I press "Sign in"
+    Then I should see "Logout"
