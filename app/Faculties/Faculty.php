ec9dbe5bac129e90135d1e4866cd11aebc85d60a
Dejan Angelov <angelovdejan92@gmail.com>
2016-07-11 01:16:43 +0200

    entity class and migration for the faculties

---
+++ app/Faculties/Faculty.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * EESTEC Platform for Local Committees
+ * Copyright (C) 2014-2016, Dejan Angelov <angelovdejan92@gmail.com>
+ *
+ * This file is part of EESTEC Platform.
+ *
+ * EESTEC Platform is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EESTEC Platform is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EESTEC Platform.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package EESTEC Platform
+ * @copyright Copyright (C) 2014-2016, Dejan Angelov <angelovdejan92@gmail.com>
+ * @license https://github.com/angelov/eestec-platform/blob/master/LICENSE
+ * @author Dejan Angelov <angelovdejan92@gmail.com>
+ */
+
+namespace Angelov\Eestec\Platform\Faculties;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Faculty extends Model
+{
+    public function getId()
+    {
+        return $this->getAttribute('id');
+    }
+
+    public function getTitle()
+    {
+        return $this->getAttribute('title');
+    }
+
+    public function setTitle($title)
+    {
+        $this->setAttribute('title', $title);
+    }
+
+    public function getAbbreviation()
+    {
+        return $this->getAttribute('abbreviation');
+    }
+
+    public function setAbbreviation($abbreviation)
+    {
+        $this->setAttribute('abbreviation', $abbreviation);
+    }
+
+    public function getUniversity()
+    {
+        return $this->getAttribute('university');
+    }
+
+    public function setUniversity($university)
+    {
+        $this->setAttribute('university', $university);
+    }
+
+    public function isEnabled()
+    {
+        return $this->getAttribute('enabled') === true;
+    }
+
+    public function setEnabled($enabled)
+    {
+        $this->setAttribute('enabled', $enabled === true);
+    }
+}
---
+++ database/migrations/2016_07_11_010958_create_faculties_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFacultiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faculties', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('abbreviation');
+            $table->string('university');
+            $table->boolean('enabled')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('faculties');
+    }
+}
