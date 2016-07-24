9c4a638bc7edc004b42e83f795982a5d3f5caded
Dejan Angelov <angelovdejan92@gmail.com>
2016-07-24 12:13:23 +0200

    showing time left for applying

--- public/static/assets/dates.js
+++ public/static/assets/dates.js
@@ -6,6 +6,17 @@ $(function(){
 
     var dateFormat = "DD MMM YYYY";
 
+    $(".time-left").each(function() {
+        var field = $(this).html();
+        var original = moment(field);
+
+        var timeLeft = original.fromNow();
+
+        timeLeft = timeLeft.replace(/in/g,"");
+
+        $(this).html(timeLeft);
+    });
+
     $(".date-to-humanize").each(function() {
         //console.log($(this).html());
         var original = $(this).html();
